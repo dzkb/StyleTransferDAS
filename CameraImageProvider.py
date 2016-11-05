b948af85dc7ffa7eb518d320f765149dcb19ad91
Bogusz Stefańczyk <stefanczykb@gmail.com>
2016-11-06 00:36:25 +0100

    added camera choosing parameter

--- CameraImageProvider.py
+++ CameraImageProvider.py
@@ -2,8 +2,9 @@ from PIL import Image, ImageTk
 import cv2
 
 class CameraImageProvider:
+    cameraID = 0 # 0 for integrated cam, 1 for first external can ....
     def __init__(self):
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(CameraImageProvider.cameraID)
     def getImagePreview(self):
         image = Image.fromarray(self.getImageRaw())
         image.thumbnail((200,200), Image.ANTIALIAS)
