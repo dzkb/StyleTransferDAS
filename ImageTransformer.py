28d6bd94130c1de13a9e7a18b928878d04808550
Bogusz Stefańczyk <stefanczykb@gmail.com>
2016-11-06 01:26:38 +0100

    added ImageTransformer

---
+++ ImageTransformer.py
@@ -0,0 +1,38 @@
+from chainer import serializers, cuda, Variable
+from net import *
+import numpy as np
+
+RUN_ON_GPU = False
+class ImageTransformer:
+    def __init__(self):
+        self.model = FastStyleNet()
+        self.modelPath = ""
+        
+    def transform(self, image, modelPath):
+        self.loadModelFromPath(modelPath)
+
+        xp = np if not RUN_ON_GPU else cuda.cupy
+
+        image = xp.asarray(image, dtype=xp.float32).transpose(2, 0, 1)
+        image = image.reshape((1,) + image.shape)
+        image -= 120
+
+        x = Variable(image)
+        y = self.model(x)
+
+        result = cuda.to_cpu(y.data)
+        result = result.transpose(0, 2, 3, 1)
+        result = result.reshape((result.shape[1:]))
+        result += 120
+        result = np.uint8(result)
+        return result
+
+
+    def loadModelFromPath(self, modelPath):
+        print("start loading")
+        if self.modelPath != modelPath:
+            serializers.load_npz(modelPath, self.model)
+            if RUN_ON_GPU:
+                cuda.get_device(0).use() #assuming only one core
+                self.model.to_gpu()
+        print("loaded")
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from Style import Style
 from Application import Application
 
 transferedArtStyles = [
-    Style("Starry night, Vincent van Gogh","images/starry-night.jpg"),
-    Style("Scream, Edvard Munch","images/scream.jpg"),
-    Style("Violin and candlestick,\nGeorges Braque", "images/violion-and-candlestick.jpg"),
-    Style("Landscape at la ciotat,\nGeorges Braque", "images/landscape-at-la-ciotat.jpg"),
-    Style("Dream, Salvadore Dali", "images/dream.jpg")
+    Style("Starry night, Vincent van Gogh",             "images/starry-night.jpg", "models/cubist.model"),
+    Style("Scream, Edvard Munch",                       "images/scream.jpg", "models/cubist.model"),
+    Style("Violin and candlestick,\nGeorges Braque",    "images/violion-and-candlestick.jpg", "models/cubist.model"),
+    Style("Landscape at la ciotat,\nGeorges Braque",    "images/landscape-at-la-ciotat.jpg", "models/cubist.model"),
+    Style("Dream, Salvadore Dali",                      "images/dream.jpg", "models/cubist.model")
 ]
 app = Application(styles=transferedArtStyles)
 app.mainloop()
